7afdc290d592eac45e5c837ee42859a6e0acaed9
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-12-01 17:24:02 -0500

    first commit

---
+++ passport.js
@@ -0,0 +1,30 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+require('dotenv').config();
+
+
+const GOOGLE_CLIENT_ID=process.env.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
